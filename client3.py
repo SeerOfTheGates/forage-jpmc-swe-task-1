d603bdafd9b8662489fe0a126e640b0dc23ca58a
SeerOfTheGates <mollyrose5555@gmail.com>
2024-07-31 21:01:56 -0500

    fixed getDataPoint and getRatio, completed getDataPoint test and added getRatio test (normal, price_b is zero, price_a is zero)

--- client3.py
+++ client3.py
@@ -35,7 +35,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price+ask_price)/2
     return stock, bid_price, ask_price, price
 
 
@@ -43,8 +43,12 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
 
-
-    return
+    if price_b == 0:
+        return
+    elif price_a == 0:
+        return
+    else:
+        return price_a / price_b
 
 
 # Main
@@ -54,10 +58,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        prices={}
+        pricesList={}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock] = price
+            pricesList[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+        print("Ratio %s" % getRatio(pricesList["ABC"], pricesList["DEF"]))
